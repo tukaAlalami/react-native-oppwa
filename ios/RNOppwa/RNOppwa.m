0acab8e121347897373a8c7fad7fc4dda6722ffe
tukaAlalami <34305635+tukaAlalami@users.noreply.github.com>
2018-12-26 00:06:16 +0300

    Update RNOppwa.m

--- ios/RNOppwa/RNOppwa.m
+++ ios/RNOppwa/RNOppwa.m
@@ -14,7 +14,7 @@ RCT_EXPORT_MODULE(RNOppwa);
       #ifdef DEBUG
         provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeTest];
      #else
-        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeLive];
+        provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeTest];
      #endif
     }
     
